01e6b0e6bf24fdc0d6d76569aa7df7c768b82ddd
EdouardSaladier <57146917+EdouardSaladier@users.noreply.github.com>
2023-02-07 11:57:06 +0100

    Initial countries research setup

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -1,3 +1,5 @@
 NDefines.NGame.START_DATE = "3376.1.1.12"
 NDefines.NGame.END_DATE = "3390.1.1.1"
 NDefines.NGame.FUEL_RESOURCE = "energy" 			-- resource that will give country fuel
+NDefines.NCountry.DEFAULT_STABILITY = 0.5,						-- Default stability if not scripted otherwise.
+NDefines.NCountry.DEFAULT_WAR_SUPPORT = 0.2,						-- Default war support if not scripted otherwise.
