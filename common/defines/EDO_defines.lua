a274f300cd6dd32af5635627053f5094db47ee82
Wolsin-Modding <71595870+Wolsin-Modding@users.noreply.github.com>
2023-02-09 14:24:19 +0100

    Revert "Merge branch 'main' of https://github.com/EdouardSaladier/Lesitia-Map"
    This reverts commit 6621d8ac4b20ee72e9276cddb547c44eda079810, reversing
    changes made to 6aae0407bf38aa9e0168280649dd91f134e83724.

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -1,6 +1,5 @@
 NDefines.NGame.START_DATE = "3376.1.1.12"
 NDefines.NGame.END_DATE = "3390.1.1.1"
 NDefines.NGame.FUEL_RESOURCE = "energy" 			-- resource that will give country fuel
-NDefines.NCountry.BASE_FUEL_GAIN_PER_OIL = 1.0
 NDefines.NCountry.DEFAULT_STABILITY = 0.5					-- Default stability if not scripted otherwise.
 NDefines.NCountry.DEFAULT_WAR_SUPPORT = 0.2						-- Default war support if not scripted otherwise.
