aacd40704c984af38a331cb87d1bee7f79970879
EdouardSaladier <57146917+EdouardSaladier@users.noreply.github.com>
2023-03-20 12:13:13 +0100

    OOB for most countries

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -1,9 +1,13 @@
 NDefines.NGame.START_DATE = "3376.1.1.12"
 NDefines.NGame.END_DATE = "3390.1.1.1"
 NDefines.NGame.FUEL_RESOURCE = "energy" 			-- resource that will give country fuel
+
 NDefines.NCountry.BASE_FUEL_GAIN_PER_OIL = 1.0
 NDefines.NCountry.DEFAULT_STABILITY = 0.5					-- Default stability if not scripted otherwise.
 NDefines.NCountry.DEFAULT_WAR_SUPPORT = 0.2						-- Default war support if not scripted otherwise.
+
+NDefines.NAI.DIVISION_MATCH_ROLE_BOOST_FACTOR = 1.5
+
 NDefines.NSupply.NUM_RAILWAYS_TRAIN_FACTOR = 0.1
 NDefines.NSupply.VP_TO_SUPPLY_BONUS_CONVERSION = 0.01
 NDefines.NSupply.VP_TO_SUPPLY_BASE = 0.05
@@ -12,3 +16,4 @@ NDefines.NSupply.NODE_FLOW_BONUS_PER_RAIL_LEVEL = 0.5
 NDefines.NSupply.INFRA_TO_SUPPLY = 0.1
 NDefines.NSupply.AVAILABLE_MANPOWER_STATE_SUPPLY = 0.05
 NDefines.NSupply.NON_CORE_MANPOWER_STATE_SUPPLY = 0.05
+
