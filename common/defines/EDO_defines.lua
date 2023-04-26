7dc7b19d81a9489c10a9ddb438fa6bcef37b7952
EdouardSaladier <57146917+EdouardSaladier@users.noreply.github.com>
2023-04-26 02:24:00 +0200

    Some FHI progress

--- common/defines/EDO_defines.lua
+++ common/defines/EDO_defines.lua
@@ -2,6 +2,8 @@ NDefines.NGame.START_DATE = "3376.1.1.12"
 NDefines.NGame.END_DATE = "3390.1.1.1"
 NDefines.NGame.FUEL_RESOURCE = "energy" 			-- resource that will give country fuel
 
+NDefines.NGame.HANDS_OFF_START_TAG = "NUK"									-- Vanilla is URG
+
 NDefines.NCountry.BASE_FUEL_GAIN_PER_OIL = 1.0
 NDefines.NCountry.DEFAULT_STABILITY = 0.5					-- Default stability if not scripted otherwise.
 NDefines.NCountry.DEFAULT_WAR_SUPPORT = 0.2						-- Default war support if not scripted otherwise.
@@ -16,4 +18,6 @@ NDefines.NSupply.NODE_FLOW_BONUS_PER_RAIL_LEVEL = 0.5
 NDefines.NSupply.INFRA_TO_SUPPLY = 0.1
 NDefines.NSupply.AVAILABLE_MANPOWER_STATE_SUPPLY = 0.05
 NDefines.NSupply.NON_CORE_MANPOWER_STATE_SUPPLY = 0.05
+
 NDefines.NCountry.BASE_STABILITY_PARTY_POPULARITY_FACTOR = 0.20	-- Default stability rulling party popularity factor
+NDefines.NCountry.MIN_STABILITY = -1.0										-- Vanilla is 0.0
